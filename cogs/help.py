f2bfc111111a7c0c249ed540e385f6b7cee1ba1b
Jzcoob Gamer <jzcoob@gmail.com>
2023-11-13 14:34:58 -0500

    Simplified Help and added `teams.py`

--- cogs/help.py
+++ cogs/help.py
@@ -14,54 +14,30 @@ class Help(commands.Cog):
     
     @app_commands.command(name="help", description="Shows the help menu!")
     async def help(self, interaction: discord.Interaction):
-        if interaction.guild.id != 1156254139966292096:
-            try:
-                embed = discord.Embed(title="Help Menu", description="Here are the commands you can use with this bot!\n\n<> = Required\n() = Not Required", color=config.color)
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1156254139966292099/1156254690573557920/61487dbbd329bb0004dbd335.png?ex=65144d98&is=6512fc18&hm=a2d4ae15d46d52bdf2e15ee6feea5042323d96b706ba03586b477b262f7af48b&")
-                embed.add_field(name="`/help`", value="Shows this help menu!", inline=False)
-                embed.add_field(name="`/player <name>`", value="Gets the information of a player!", inline=False)
-                embed.add_field(name="`/guess-the-player`", value="Guess the player!", inline=False)
-                embed.add_field(name="`/guess-the-team`", value="Guess the team!", inline=False)
-                embed.add_field(name="`/team <name>`", value="Gets the information of a team!", inline=False)
-                embed.add_field(name="`/standings`", value="Gets the standings of the NHL!", inline=False)
-                embed.add_field(name="`/schedule <abbreviation>`", value="Gets the schedule of the NHL!", inline=False)
-                embed.add_field(name="`/game <team>`", value="Gets the information of a game!", inline=False)
-                embed.add_field(name="`/today`", value="Gets the games of today!", inline=False)
-                embed.add_field(name="`/help`", value="Shows this help menu!", inline=False)
-                embed.set_footer(text="Made by @jzcob")
-                await interaction.response.send_message(embed=embed, ephemeral=True)
-            except Exception as e:
-                error_channel = self.bot.get_channel(config.error_channel)
-                embed = discord.Embed(title="Error with `/help`", description=f"```{e}```", color=config.color)
-                embed.add_field(name="User", value=f"{interaction.user.mention}", inline=False)
-                embed.add_field(name="Server", value=f"{interaction.guild.name}", inline=False)
-                embed.add_field(name="Channel", value=f"{interaction.channel.name}", inline=False)
-                await error_channel.send(embed=embed)
-        else:
-            #TEMPORARY
-            try:
-                embed = discord.Embed(title="Help Menu", description="Here are the commands you can use with this bot!\n\n<> = Required\n() = Not Required", color=config.color)
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1156254139966292099/1156254690573557920/61487dbbd329bb0004dbd335.png?ex=65144d98&is=6512fc18&hm=a2d4ae15d46d52bdf2e15ee6feea5042323d96b706ba03586b477b262f7af48b&")
-                embed.add_field(name="`/help`", value="Shows this help menu!", inline=False)
-                embed.add_field(name="`/player <name>`", value="Gets the information of a player!", inline=False)
-                embed.add_field(name="`/guess-the-player`", value="Guess the player!", inline=False)
-                embed.add_field(name="`/guess-the-team`", value="Guess the team!", inline=False)
-                embed.add_field(name="`/team <name>`", value="Gets the information of a team!", inline=False)
-                embed.add_field(name="`/standings`", value="Gets the standings of the NHL!", inline=False)
-                embed.add_field(name="`/schedule <abbreviation>`", value="Gets the schedule of the NHL!", inline=False)
-                embed.add_field(name="`/game <team>`", value="Gets the information of a game!", inline=False)
-                embed.add_field(name="`/today`", value="Gets the games for today!", inline=False)
-                embed.add_field(name="`/help`", value="Shows this help menu!", inline=False)
-                embed.set_footer(text="Made by @jzcob")
-                await interaction.response.send_message(embed=embed, ephemeral=True)
-            except Exception as e:
-                error_channel = self.bot.get_channel(config.error_channel)
-                embed = discord.Embed(title="Error with `/help`", description=f"```{e}```", color=config.color)
-                embed.add_field(name="User", value=f"{interaction.user.mention}", inline=False)
-                embed.add_field(name="Server", value=f"{interaction.guild.name}", inline=False)
-                embed.add_field(name="Channel", value=f"{interaction.channel.name}", inline=False)
-                await error_channel.send(embed=embed)
-    
+        try:
+            embed = discord.Embed(title="Help Menu", description="Here are the commands you can use with this bot!\n\n<> = Required\n() = Not Required", color=config.color)
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1156254139966292099/1156254690573557920/61487dbbd329bb0004dbd335.png?ex=65144d98&is=6512fc18&hm=a2d4ae15d46d52bdf2e15ee6feea5042323d96b706ba03586b477b262f7af48b&")
+            embed.add_field(name="`/help`", value="Shows this help menu!", inline=False)
+            #embed.add_field(name="`/player <name>`", value="Gets the information of a player!", inline=False)
+            #embed.add_field(name="`/guess-the-player`", value="Guess the player!", inline=False)
+            embed.add_field(name="`/guess-the-team`", value="Guess the team!", inline=False)
+            embed.add_field(name="`/team <name>`", value="Gets the information of a team!", inline=False)
+            embed.add_field(name="`/teams`", value="Gets the teams in the NHL!", inline=False)
+            embed.add_field(name="`/standings`", value="Gets the standings of the NHL!", inline=False)
+            embed.add_field(name="`/schedule <abbreviation>`", value="Gets the schedule of the NHL!", inline=False)
+            embed.add_field(name="`/game <abbreviation>`", value="Gets the information of a game!", inline=False)
+            embed.add_field(name="`/today`", value="Gets the games of today!", inline=False)
+            embed.add_field(name="`/help`", value="Shows this help menu!", inline=False)
+            embed.set_footer(text="Made by @jzcob")
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+        except Exception as e:
+            error_channel = self.bot.get_channel(config.error_channel)
+            embed = discord.Embed(title="Error with `/help`", description=f"```{e}```", color=config.color)
+            embed.add_field(name="User", value=f"{interaction.user.mention}", inline=False)
+            embed.add_field(name="Server", value=f"{interaction.guild.name}", inline=False)
+            embed.add_field(name="Channel", value=f"{interaction.channel.name}", inline=False)
+            await error_channel.send(embed=embed)
+
 
 async def setup(bot):
     await bot.add_cog(Help(bot))
---
+++ cogs/teams.py
@@ -0,0 +1,66 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import requests
+from datetime import datetime, timedelta
+import config
+
+class teams(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"LOADED: `teams.py`")
+    
+    @app_commands.command(name="teams", description="Get the teams in the league!")
+    async def teams(self, interaction: discord.Interaction):
+        teams = {
+            "ANA": "Anaheim Ducks",
+            "ARI": "Arizona Coyotes",
+            "BOS": "Boston Bruins",
+            "BUF": "Buffalo Sabres",
+            "CGY": "Calgary Flames",
+            "CAR": "Carolina Hurricanes",
+            "CHI": "Chicago Blackhawks",
+            "COL": "Colorado Avalanche",
+            "CBJ": "Columbus Blue Jackets",
+            "DAL": "Dallas Stars",
+            "DET": "Detroit Red Wings",
+            "EDM": "Edmonton Oilers",
+            "FLA": "Florida Panthers",
+            "LAK": "Los Angeles Kings",
+            "MIN": "Minnesota Wild",
+            "MTL": "Montreal Canadiens",
+            "NSH": "Nashville Predators",
+            "NJD": "New Jersey Devils",
+            "NYI": "New York Islanders",
+            "NYR": "New York Rangers",
+            "OTT": "Ottawa Senators",
+            "PHI": "Philadelphia Flyers",
+            "PIT": "Pittsburgh Penguins",
+            "SJS": "San Jose Sharks",
+            "STL": "St. Louis Blues",
+            "TBL": "Tampa Bay Lightning",
+            "TOR": "Toronto Maple Leafs",
+            "VAN": "Vancouver Canucks",
+            "VGK": "Vegas Golden Knights",
+            "WSH": "Washington Capitals",
+            "WPG": "Winnipeg Jets"
+        }
+    
+        try:
+            embed = discord.Embed(title="Teams", description=f"Here are the teams in the league!", color=0x00ff00)
+            for team in teams:
+                embed.add_field(name=team, value=teams[team], inline=False)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+            return
+        except Exception as e:
+            print(e)
+            error_channel = self.get_channel(config.error_channel)
+            await interaction.response.send_message("Error processing command. Message has been sent to Bot Developers", ephemeral=True)
+            await error_channel.send(f"Error in command `/teams`: {e}")
+            return
+
+async def setup(bot):
+    await bot.add_cog(teams(bot))
