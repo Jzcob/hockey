ee481b6a1d90d2548beb77bcfb6f2a449a6d6a3a
Jzcoob Gamer <jzcoob@gmail.com>
2023-11-18 23:47:16 -0500

    Create avatar.py

---
+++ cogs/avatar.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class avatar(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"LOADED: `avatar.py`")
+    
+    @app_commands.command(name="avatar", description="Get the bot's avatar!")
+    async def avatar(self, interaction: discord.Interaction):
+        try:
+            await interaction.response.defer()
+            msg = await interaction.original_response()
+            embed = discord.Embed(title="Avatar", url="https://www.craiyon.com/image/srRf0fglTs-NwjWKUC3vNg", color=0x00ff00)
+            embed.set_image(url=self.bot.user.avatar_url)
+            await msg.edit(embed=embed)
+        except Exception as e:
+            print(e)
