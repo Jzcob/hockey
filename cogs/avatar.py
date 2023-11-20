857223a0391811ea5405b64780e516f7baeb69bf
Jzcoob Gamer <jzcoob@gmail.com>
2023-11-20 12:02:15 -0500

    Update avatar.py

--- cogs/avatar.py
+++ cogs/avatar.py
@@ -12,13 +12,20 @@ class avatar(commands.Cog):
         print(f"LOADED: `avatar.py`")
     
     @app_commands.command(name="avatar", description="Get the bot's avatar!")
-    async def avatar(self, interaction: discord.Interaction):
+    async def avatar(self, interaction: discord.Interaction, user: discord.User = None):
         try:
-            await interaction.response.defer()
-            msg = await interaction.original_response()
-            embed = discord.Embed(title="Avatar", url="https://www.craiyon.com/image/srRf0fglTs-NwjWKUC3vNg", color=0x00ff00)
-            embed.set_image(url=self.bot.user.avatar)
-            await msg.edit(embed=embed)
+            if user == None:
+                await interaction.response.defer()
+                msg = await interaction.original_response()
+                embed = discord.Embed(title="Avatar", url="https://www.craiyon.com/image/srRf0fglTs-NwjWKUC3vNg", color=0x00ff00)
+                embed.set_image(url=self.bot.user.avatar)
+                await msg.edit(embed=embed)
+            else:
+                await interaction.response.defer()
+                msg = await interaction.original_response()
+                embed = discord.Embed(title="Avatar", url=user.avatar, color=0x00ff00)
+                embed.set_image(url=user.avatar)
+                await msg.edit(embed=embed)
         except Exception as e:
             print(e)
 
