3ccbddf8a9c1f8bdc7d21256123f4a69be2b4d74
Jzcob <jzcoob@gmail.com>
2023-11-22 10:36:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ intents.auto_moderation_configuration = True
 intents.reactions = True
 bot = commands.Bot(command_prefix=';', intents=intents, help_command=None)
 status = discord.Status.online
-activity = discord.Activity(name="hockey", type=discord.ActivityType.watching)
+watching = discord.Activity(name="hockey", type=discord.ActivityType.watching)
 
 
 @bot.command()
@@ -89,7 +89,7 @@ async def load():
 async def main():
     await load()
     await bot.start(token=TOKEN)
-    await bot.change_presence(activity=activity)
+    await bot.change_presence(activity=watching)
 
 @bot.event
 async def on_guild_join(guild):
