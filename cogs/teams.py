ef4d650f048e893ce95d945feb0e4264c7b487b5
Jzcob <jzcoob@gmail.com>
2023-11-25 16:47:45 -0500

    Made teams looks cooler!

--- cogs/teams.py
+++ cogs/teams.py
@@ -50,7 +50,69 @@ class teams(commands.Cog):
         try:
             embed = discord.Embed(title="Teams", description=f"Here are the teams in the league!\n\n", color=0x00ff00)
             for team in teams:
-                embed.description += f"**{team}** - {teams[team]}\n"
+                if team == "ANA":
+                    embed.description += f"**{team}** - {teams[team]} {config.anahiem_ducks_emoji}\n"
+                elif team == "ARI":
+                    embed.description += f"**{team}** - {teams[team]} {config.arizona_coyotes_emoji}\n"
+                elif team == "BOS":
+                    embed.description += f"**{team}** - {teams[team]} {config.boston_bruins_emoji}\n"
+                elif team == "BUF":
+                    embed.description += f"**{team}** - {teams[team]} {config.buffalo_sabres_emoji}\n"
+                elif team == "CGY":
+                    embed.description += f"**{team}** - {teams[team]} {config.calgary_flames_emoji}\n"
+                elif team == "CAR":
+                    embed.description += f"**{team}** - {teams[team]} {config.carolina_hurricanes_emoji}\n"
+                elif team == "CHI":
+                    embed.description += f"**{team}** - {teams[team]} {config.chicago_blackhawks_emoji}\n"
+                elif team == "COL":
+                    embed.description += f"**{team}** - {teams[team]} {config.colorado_avalanche_emoji}\n"
+                elif team == "CBJ":
+                    embed.description += f"**{team}** - {teams[team]} {config.columbus_blue_jackets_emoji}\n"
+                elif team == "DAL":
+                    embed.description += f"**{team}** - {teams[team]} {config.dallas_stars_emoji}\n"
+                elif team == "DET":
+                    embed.description += f"**{team}** - {teams[team]} {config.detroit_red_wings_emoji}\n"
+                elif team == "EDM":
+                    embed.description += f"**{team}** - {teams[team]} {config.edmonton_oilers_emoji}\n"
+                elif team == "FLA":
+                    embed.description += f"**{team}** - {teams[team]} {config.florida_panthers_emoji}\n"
+                elif team == "LAK":
+                    embed.description += f"**{team}** - {teams[team]} {config.los_angeles_kings_emoji}\n"
+                elif team == "MIN":
+                    embed.description += f"**{team}** - {teams[team]} {config.minnesota_wild_emoji}\n"
+                elif team == "MTL":
+                    embed.description += f"**{team}** - {teams[team]} {config.montreal_canadiens_emoji}\n"
+                elif team == "NSH":
+                    embed.description += f"**{team}** - {teams[team]} {config.nashville_predators_emoji}\n"
+                elif team == "NJD":
+                    embed.description += f"**{team}** - {teams[team]} {config.new_jersey_devils_emoji}\n"
+                elif team == "NYI":
+                    embed.description += f"**{team}** - {teams[team]} {config.new_york_islanders_emoji}\n"
+                elif team == "NYR":
+                    embed.description += f"**{team}** - {teams[team]} {config.new_york_rangers_emoji}\n"
+                elif team == "OTT":
+                    embed.description += f"**{team}** - {teams[team]} {config.ottawa_senators_emoji}\n"
+                elif team == "PHI":
+                    embed.description += f"**{team}** - {teams[team]} {config.philadelphia_flyers_emoji}\n"
+                elif team == "PIT":
+                    embed.description += f"**{team}** - {teams[team]} {config.pittsburgh_penguins_emoji}\n"
+                elif team == "SJS":
+                    embed.description += f"**{team}** - {teams[team]} {config.san_jose_sharks_emoji}\n"
+                elif team == "STL":
+                    embed.description += f"**{team}** - {teams[team]} {config.st_louis_blues_emoji}\n"
+                elif team == "TBL":
+                    embed.description += f"**{team}** - {teams[team]} {config.tampa_bay_lightning_emoji}\n"
+                elif team == "TOR":
+                    embed.description += f"**{team}** - {teams[team]} {config.toronto_maple_leafs_emoji}\n"
+                elif team == "VAN":
+                    embed.description += f"**{team}** - {teams[team]} {config.vancouver_canucks_emoji}\n"
+                elif team == "VGK":
+                    embed.description += f"**{team}** - {teams[team]} {config.vegas_golden_knights_emoji}\n"
+                elif team == "WSH":
+                    embed.description += f"**{team}** - {teams[team]} {config.washington_capitals_emoji}\n"
+                elif team == "WPG":
+                    embed.description += f"**{team}** - {teams[team]} {config.winnipeg_jets_emoji}\n"
+            embed.set_author(icon_url=interaction.user.avatar.url, name="NHL Bot")
             embed.set_footer(text=config.footer)
             await interaction.response.send_message(embed=embed, ephemeral=True)
             return
--- config.py
+++ config.py
@@ -15,6 +15,42 @@ color = 0xffffff
 error_channel = 1166019404870463558
 footer = "Bot created by @jzcob"
 
+
+anahiem_ducks_emoji = "<:anaheimducks:1166050853879296030>"
+arizona_coyotes_emoji = "<:arizonacoyotes:1166050957740337664>"
+boston_bruins_emoji = "<:bostonbruins:1166051061666307584>"
+buffalo_sabres_emoji = "<:buffalosabres:1166051165541379072>"
+calgary_flames_emoji = "<:calgaryflames:1166051546337923112>"
+carolina_hurricanes_emoji = "<:carolinahurricanes:1166051616483455087>"
+chicago_blackhawks_emoji = "<:chicagoblackhawks:1178009419968151583>"
+colorado_avalanche_emoji = "<:coloradoavalance:1178009564201877514>"
+columbus_blue_jackets_emoji = "<:columbusbluejackets:1178009622821474426> "
+dallas_stars_emoji = "<:dallasstars:1178009644661231616> "
+detroit_red_wings_emoji = "<:detroitredwings:1178010979519438848>"
+edmonton_oilers_emoji = "<:edmontonoilers:1178011001153654814> "
+florida_panthers_emoji = "<:floridapanthers:1178011019340165281> "
+los_angeles_kings_emoji = "<:losangeleskings:1178011041989414952>"
+minnesota_wild_emoji = "<:minnesotawild:1178011065632698489>"
+montreal_canadiens_emoji = "<:montrealcanadiens:1178011086792958043>"
+nashville_predators_emoji = "<:nashvillepredators:1178011107785461860>"
+new_jersey_devils_emoji = "<:newjersydevils:1178083568350679170>"
+new_york_islanders_emoji = "<:newyorkislanders:1178083587552198758>"
+new_york_rangers_emoji = "<:newyorkrangers:1178083606866952202>"
+ottawa_senators_emoji = "<:ottawasenators:1178083784420245616>"
+philadelphia_flyers_emoji = "<:philadelphiaflyers:1178083819887267930>"
+pittsburgh_penguins_emoji = "<:pittsburghpenguins:1178085887968555128>"
+san_jose_sharks_emoji = "<:sanjosesharks:1178085931861942363>"
+st_louis_blues_emoji = "<:stlouisblues:1178085954368589825>"
+tampa_bay_lightning_emoji = "<:tampabaylightning:1178085977189793802>"
+toronto_maple_leafs_emoji = "<:torontomapleleafs:1178086004616339616>"
+vancouver_canucks_emoji = "<:vancouvercanucks:1178086027353665626>"
+vegas_golden_knights_emoji = "<:vegasgoldenknights:1178086044931989576>"
+washington_capitals_emoji = "<:washingtoncapitals:1178086062518710413>"
+winnipeg_jets_emoji = "<:winnipegjets:1178086107418722434>"
+nhl_logo_emoji = "<:nhl:1165874790117150820>"
+
+
+
 def checkForTeam(team: str) -> None:
     url = "https://statsapi.web.nhl.com/api/v1/teams"
     response = requests.get(url)
