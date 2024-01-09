11f2d8c6e38d7669901e801807cd03e06c9a922f
Jzcob <jzcoob@gmail.com>
2024-01-08 22:57:55 -0500

    Update team.py

--- cogs/team.py
+++ cogs/team.py
@@ -82,9 +82,9 @@ class team(commands.Cog):
                         streakCode = data['standings'][i]['streakCode']
                         streakNumber = data['standings'][i]['streakCount']
                         teamLogo = data['standings'][i]['teamLogo']
-                        renderPM.drawToFile(svg2rlg(teamLogo), "teamLogo.png", fmt="PNG")
+                        image = renderPM.drawToFile(svg2rlg(teamLogo), "teamLogo.png", fmt="PNG")
                         embed = discord.Embed(description=f"**{confrence} confrence & {division} division**", color=config.color)
-                        embed.thumbnail(url="teamLogo.png")
+                        embed.thumbnail(image)
                         if teamAbbreviation == "ANA":
                             embed.title = f"{teamName} ({teamAbbreviation}) {config.anahiem_ducks_emoji}"
                         elif teamAbbreviation == "ARI":
