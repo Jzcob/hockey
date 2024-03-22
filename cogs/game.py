37825e88f1e667c6efec1874c3bcf4c864d31f6e
Jzcoob Gamer <jzcoob@gmail.com>
2024-03-22 01:55:51 -0400

    Update game.py

--- cogs/game.py
+++ cogs/game.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from discord.ui import Button, View
 from discord import app_commands
 import requests
 from datetime import datetime, timedelta
@@ -9,46 +8,6 @@ import pytz
 import traceback
 
 gameIDs = {}
-
-gameButton = Button(label="Game Reports", style=discord.ButtonStyle.success)
-
-async def gameButton_callback(interaction):
-    try:
-        if interaction.user.id in gameIDs:
-            url = f"https://api-web.nhle.com/v1/gamecenter/{gameIDs[interaction.user.id]}/boxscore"
-            response = requests.get(url)
-            data = response.json()
-            home = data['homeTeam']['name']['default']
-            away = data['awayTeam']['name']['default']
-            gameReports = data['boxscore']['gameReports']
-            gameSummary = gameReports['gameSummary']
-            gameEvents = gameReports['eventSummary']
-            playByPlay = gameReports['playByPlay']
-            faceOffSummary = gameReports['faceoffSummary']
-            faceOffComparison = gameReports['faceoffComparison']
-            rosters = gameReports['rosters']
-            shotSummary = gameReports['shotSummary']
-            toiAway = gameReports['toiAway']
-            toiHome = gameReports['toiHome']
-            embed = discord.Embed(title=f"Game Reports for {away} @ {home}", color=config.color)
-            embed.add_field(name="Game Summary", value=gameSummary, inline=False)
-            embed.add_field(name="Game Events", value=gameEvents, inline=False)
-            embed.add_field(name="Play By Play", value=playByPlay, inline=False)
-            embed.add_field(name="Faceoff Summary", value=faceOffSummary, inline=False)
-            embed.add_field(name="Faceoff Comparison", value=faceOffComparison, inline=False)
-            embed.add_field(name="Rosters", value=rosters, inline=False)
-            embed.add_field(name="Shot Summary", value=shotSummary, inline=False)
-            embed.add_field(name="Time on Ice Away", value=toiAway, inline=False)
-            embed.add_field(name="Time on Ice Home", value=toiHome, inline=False)
-            embed.set_footer(text=config.footer)
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-            gameIDs.pop(interaction.user.id)
-        else:
-            await interaction.response.send_message("Please use the `/game` command first!", ephemeral=True)
-    except Exception as e:
-        error_channel = interaction.bot.get_channel(config.error_channel)
-        string = f"{traceback.format_exc()}"
-        await error_channel.send(f"```{string}```")
 
 class game(commands.Cog):
     def __init__(self, bot):
@@ -103,8 +62,6 @@ class game(commands.Cog):
             else:
                 await msg.edit(content="Please enter a valid team abbreviation. e.g. `/game BOS`")
                 return
-            view = View()
-            view.add_item(gameButton)
             hawaii = pytz.timezone('US/Hawaii')
             dt = datetime.now(hawaii)
             today = dt.strftime('%Y-%m-%d')
@@ -147,26 +104,23 @@ class game(commands.Cog):
                 embed.set_footer(text=config.footer)
                 await msg.edit(embed=embed)
                 return
-            homeScore = data2['boxscore']['linescore']['totals']['home']
-            awayScore = data2['boxscore']['linescore']['totals']['away']
+            homeScore = data2['hometeam']['score']
+            awayScore = data2['awayteam']['score']
             clock = data2['clock']['timeRemaining']
             clockRunning = data2['clock']['running']
             clockIntermission = data2['clock']['inIntermission']
-            shotsByPeriod = data2['boxscore']['shotsByPeriod']
             homeShots = 0
             awayShots = 0
             if game['gameState'] == "FINAL" or game['gameState'] == "OFF":
                 embed = discord.Embed(title=f"{away} @ {home}", description=f"Final!\nScore: {awayScore} | {homeScore}", color=config.color)
                 
                 embed.set_footer(text=config.footer)
-                await msg.edit(embed=embed, view=view)
-                gameButton.callback = gameButton_callback
+                await msg.edit(embed=embed)
                 return
             embed = discord.Embed(title=f"{away} @ {home}", description=f"{awayScore} - {homeScore}", color=config.color)
             
-            for i in range(len(shotsByPeriod)):
-                homeShots += shotsByPeriod[i]['home']
-                awayShots += shotsByPeriod[i]['away']
+            homeShots = data2['homeTeam']['sog']
+            awayShots = data2['awayTeam']['sog']
             embed.description = f"GAME IS LIVE!!!\n\nScore: {awayScore} - {homeScore}\nShots: {awayShots} - {homeShots}"
             if clockRunning == True:
                 embed.add_field(name="Clock", value=f"{clock}\nRunning", inline=False)
@@ -177,8 +131,7 @@ class game(commands.Cog):
             embed.set_footer(text=config.footer)
             embed.add_field(name="TV Broadcast", value=f"{networks}", inline=False)
             embed.add_field(name="Game ID", value=gameID, inline=False)
-            await msg.edit(embed=embed, view=view)
-            gameButton.callback = gameButton_callback
+            await msg.edit(embed=embed)
         except:
             error_channel = self.bot.get_channel(config.error_channel)
             string = f"{traceback.format_exc()}"
