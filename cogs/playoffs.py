7c28f2861cbddb377d243aa4a3e357e8d5903d60
Jzcob <jzcoob@gmail.com>
2024-05-17 23:35:18 -0400

    Quick update

--- cogs/playoffs.py
+++ cogs/playoffs.py
@@ -23,9 +23,9 @@ class playoffs(commands.Cog):
             embed.add_field(name="Round 1", value="**Eastern**\nFlorida Panthers vs Tampa Bay Lightning **(4-1)**\nBoston Bruins vs Toronto Maple Leafs **(4-3)**"+
                             "\nNew York Rangers vs Washing Capitals **(4-0)**\nCarolina Hurricanes vs New York Islanders **(4-1)**\n**Western**\nDallas Stars vs Vegas Golden Knights **(4-3)**\n"+
                             "Winnipeg Jets vs Colorado Avalanche **(1-4)**\nVancover Canucks vs Nashville Predators **(4-2)**\n Edmonton Oilers vs Los Angeles Kings **(4-1)**", inline=False)
-            embed.add_field(name="Round 2", value="**Eastern**\nFlordia Panthers vs Boston Bruins **(3-2)**\nNew York Rangers vs Carolina Hurricanes **(3-2)**\n**Western**\nDallas Stars vs Colorado Avalanche **(3-1)**\n"+
-                            "Vancover Canucks vs Edmonton Oilers **(2-2)**", inline=False)
-            embed.add_field(name="Conference Finals", value="**Eastern**\n vs TBD\n**Western**\nTBD vs TBD", inline=False)
+            embed.add_field(name="Round 2", value="**Eastern**\nFlordia Panthers vs Boston Bruins **(4-2)**\nNew York Rangers vs Carolina Hurricanes **(4-2)**\n**Western**\nDallas Stars vs Colorado Avalanche **(3-2)**\n"+
+                            "Vancover Canucks vs Edmonton Oilers **(3-2)**", inline=False)
+            embed.add_field(name="Conference Finals", value="**Eastern**\nNew York Rangers vs Flordia Panthers\n**Western**\nTBD vs TBD", inline=False)
             embed.add_field(name="Stanley Cup Finals", value="TBD vs TBD", inline=False)
                 
             await interaction.response.send_message(embed=embed)
--- main.py
+++ main.py
@@ -110,9 +110,10 @@ async def on_guild_join(guild):
                                             f"Members: {guild.member_count}\n", color=0x00ff00)
         embed.set_thumbnail(url=f"{guild.icon}")
 
-        await join_leave_channel.send(embed=embed)
+        global msg 
+        msg = await join_leave_channel.send(embed=embed)
     except:
-        await join_leave_channel.send("Joined a server but couldn't get server information")
+        msg = await join_leave_channel.send("Joined a server but couldn't get server information")
     try:
         guilds = bot.guilds
         members = 0
@@ -124,6 +125,15 @@ async def on_guild_join(guild):
         await vc.edit(name=f"Servers: {len(bot.guilds)}")
         if len(bot.guilds) % 100 == 0:
             await join_leave_channel.send(f"<@920797181034778655> Bot has reached a milestone of `{len(bot.guilds)}` servers!")
+        try:
+            embed = discord.Embed(title="Thank you for adding me!", description="This is a warning that once the playoffs are over, some of the commands of the bot will be offline until the next season starts. If you have any questions, feel free to join our support server with `/info`\n\n### Commands that will be disabled once the season has ended:\n- `/standings`\n- `/today`\n- `/yesterday`\n- `/game`\n- `/schedule`\n- `/series`")
+            try:
+                await guild.public_updates_channel.send(embed=embed)
+            except Exception as e:
+                await guild.system_channel.send(embed=embed)
+            await msg.add_reaction("👍")
+        except:
+            await msg.add_reaction("👎")
     except Exception as e:
         print(e)
 
