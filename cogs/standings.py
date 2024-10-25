e730ad192b9ea003b34da901ecc0374be3c65349
Jzcob <jzcoob@gmail.com>
2024-10-25 15:27:40 -0400

    Update standings.py

--- cogs/standings.py
+++ cogs/standings.py
@@ -6,6 +6,78 @@ from datetime import datetime
 import config
 import traceback
 
+def strings(name):
+    if name == "Anaheim Ducks":
+        string = f"{config.anahiem_ducks_emoji} {name}"
+    elif name == "Boston Bruins":
+        string = f"{config.boston_bruins_emoji} {name}"
+    elif name == "Buffalo Sabres":
+        string = f"{config.buffalo_sabres_emoji} {name}"
+    elif name == "Calgary Flames":
+        string = f"{config.calgary_flames_emoji} {name}"
+    elif name == "Carolina Hurricanes":
+        string = f"{config.carolina_hurricanes_emoji} {name}"
+    elif name == "Chicago Blackhawks":
+        string = f"{config.chicago_blackhawks_emoji} {name}"
+    elif name == "Colorado Avalanche":
+        string = f"{config.colorado_avalanche_emoji} {name}"
+    elif name == "Columbus Blue Jackets":
+        string = f"{config.columbus_blue_jackets_emoji} {name}"
+    elif name == "Dallas Stars":
+        string = f"{config.dallas_stars_emoji} {name}"
+    elif name == "Detroit Red Wings":
+        string = f"{config.detroit_red_wings_emoji} {name}"
+    elif name == "Edmonton Oilers":
+        string = f"{config.edmonton_oilers_emoji} {name}"
+    elif name == "Florida Panthers":
+        string = f"{config.florida_panthers_emoji} {name}"
+    elif name == "Los Angeles Kings":
+        string = f"{config.los_angeles_kings_emoji} {name}"
+    elif name == "Minnesota Wild":
+        string = f"{config.minnesota_wild_emoji} {name}"
+    elif name == "Montreal Canadiens":
+        string = f"{config.montreal_canadiens_emoji} {name}"
+    elif name == "Nashville Predators":
+        string = f"{config.nashville_predators_emoji} {name}"
+    elif name == "New Jersey Devils":
+        string = f"{config.new_jersey_devils_emoji} {name}"
+    elif name == "New York Islanders":
+        string = f"{config.new_york_islanders_emoji} {name}"
+    elif name == "New York Rangers":
+        string = f"{config.new_york_rangers_emoji} {name}"
+    elif name == "Ottawa Senators":
+        string = f"{config.ottawa_senators_emoji} {name}"
+    elif name == "Philadelphia Flyers":
+        string = f"{config.philadelphia_flyers_emoji} {name}"
+    elif name == "Pittsburgh Penguins":
+        string = f"{config.pittsburgh_penguins_emoji} {name}"
+    elif name == "San Jose Sharks":
+        string = f"{config.san_jose_sharks_emoji} {name}"
+    elif name == "St. Louis Blues":
+        string = f"{config.st_louis_blues_emoji} {name}"
+    elif name == "Tampa Bay Lightning":
+        string = f"{config.tampa_bay_lightning_emoji} {name}"
+    elif name == "Toronto Maple Leafs":
+        string = f"{config.toronto_maple_leafs_emoji} {name}"
+    elif name == "Vancouver Canucks":
+        string = f"{config.vancouver_canucks_emoji} {name}"
+    elif name == "Vegas Golden Knights":
+        string = f"{config.vegas_golden_knights_emoji} {name}"
+    elif name == "Washington Capitals":
+        string = f"{config.washington_capitals_emoji} {name}"
+    elif name == "Winnipeg Jets":
+        string = f"{config.winnipeg_jets_emoji} {name}"
+    elif name == "Seattle Kraken":
+        string = f"{config.seattle_kraken_emoji} {name}"
+    elif name == "Utah Hockey Club":
+        string = f"{config.utah_hockey_club_emoji} {name}"
+    else:
+        string = name
+
+    return string
+
+
+
 class standings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -46,8 +118,9 @@ class standings(commands.Cog):
                     losses = data['standings'][i]['losses']
                     otLosses = data['standings'][i]['otLosses']
                     points = data['standings'][i]['points']
-                    atlanticString += data['standings'][i]['teamName']['default']
-                    atlanticString += f" ({wins}-{losses}-{otLosses}) {points}pts"
+                    name = data['standings'][i]['teamName']['default']
+                    new_name = strings(name)
+                    atlanticString += f"{new_name} ({wins}-{losses}-{otLosses}) {points}pts"
                     atlantic.append(atlanticString)
                 elif divisionName == "Metropolitan":
                     metropolitanString = ""
@@ -55,8 +128,9 @@ class standings(commands.Cog):
                     losses = data['standings'][i]['losses']
                     otLosses = data['standings'][i]['otLosses']
                     points = data['standings'][i]['points']
-                    metropolitanString += data['standings'][i]['teamName']['default']
-                    metropolitanString += f" ({wins}-{losses}-{otLosses}) {points}pts"
+                    name = data['standings'][i]['teamName']['default']
+                    new_name = strings(name)
+                    metropolitanString += f"{new_name} ({wins}-{losses}-{otLosses}) {points}pts"
                     metropolitan.append(metropolitanString)
                 elif divisionName == "Central":
                     centralString = ""
@@ -64,8 +138,9 @@ class standings(commands.Cog):
                     losses = data['standings'][i]['losses']
                     otLosses = data['standings'][i]['otLosses']
                     points = data['standings'][i]['points']
-                    centralString += data['standings'][i]['teamName']['default']
-                    centralString += f" ({wins}-{losses}-{otLosses}) {points}pts"
+                    name = data['standings'][i]['teamName']['default']
+                    new_name = strings(name)
+                    centralString += f"{new_name} ({wins}-{losses}-{otLosses}) {points}pts"
                     central.append(centralString)
                 elif divisionName == "Pacific":
                     pacificString = ""
@@ -73,8 +148,9 @@ class standings(commands.Cog):
                     losses = data['standings'][i]['losses']
                     otLosses = data['standings'][i]['otLosses']
                     points = data['standings'][i]['points']
-                    pacificString += data['standings'][i]['teamName']['default']
-                    pacificString += f" ({wins}-{losses}-{otLosses}) {points}pts"
+                    name = data['standings'][i]['teamName']['default']
+                    new_name = strings(name)
+                    pacificString += f"{new_name} ({wins}-{losses}-{otLosses}) {points}pts"
                     pacific.append(pacificString)
             embed = discord.Embed(title="Standings", color=config.color)
             embed.set_thumbnail(url="https://www-league.nhlstatic.com/images/logos/league-dark/133-flat.svg")
